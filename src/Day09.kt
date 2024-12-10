d9640b22aaf4abab75e2a7950483a3705b280d10
isabelle_sniper <minhthong0411@yahoo.com>
2024-12-09 23:29:36 -0800

    Day 09 (complete)

--- src/Day09.kt
+++ src/Day09.kt
@@ -54,8 +54,98 @@ fun main() {
         return result
     }
 
-    fun part2(input: List<String>): Int {
-        var result = 0;
+    fun part2(input: List<String>): ULong {
+        var result = 0uL;
+        for(diskMap in input) {
+            val originalMap = diskMap.toDiskBlock()
+            val block = originalMap.reversed()
+            val windows: MutableList<List<Int>> = mutableListOf()
+            var temp = listOf<Int>()
+            for (i in 0 until block.size - 1) {
+                if (block[i] != -1 && block[i] == block[i + 1]) {
+                    temp = temp.plus(block[i])
+                } else {
+                    if (block[i] != -1) {
+                        temp = temp.plus(block[i])
+                    }
+                    if (temp.isNotEmpty()) {
+                        windows.add(temp)
+                    }
+                    temp = listOf()
+                }
+            }
+
+//            val map = mutableListOf<Int>()
+//            map.addAll(originalMap)
+//            println(windows)
+//            println(originalMap)
+//            println(map)
+
+            for(window in windows) {
+                //println("WINDOW $window")
+                var i = 0
+                val idx = originalMap.indexOf(window[0])
+                while (i + window.size - 1 < originalMap.size && i < idx) {
+                    //println("CURR I: $i")
+                    if(originalMap[i] == -1) {
+                        var doesFit = true
+                        //println("INSIDE " + i + " ${window.size}")
+                        for(k in i until i + window.size){
+                            //println("k $k ${originalMap[k] == -1}")
+                            if(originalMap[k] != -1) {
+                                doesFit = false
+                                break
+                            }
+                        }
+                        //println("FIT? $doesFit")
+
+                        if(doesFit) {
+                            for(k in idx until idx + window.size) {
+                                originalMap[k] = -1
+                            }
+                            for(k in i until i + window.size){
+                                originalMap[k] = window[0]
+                            }
+                            //println(originalMap)
+                            //i += window.size
+                            //println(i)
+                            break
+                        } else {
+                            ++i
+                        }
+                    }else {
+                        ++i
+                    }
+
+//                    var doesFit = true
+//                    //println("i: $i")
+//                    if (window.size > 1) {
+//                        for (j in 0 until window.lastIndex) {
+//                            //println("j: $j")
+//                            //println("${map[i + j]} ${map[i + j + 1]}")
+//                            if (map[i + j] != -1 || map[i + j + 1] != -1) {
+//                                doesFit = false
+//                                break
+//                            }
+//                        }
+//                    } else {
+//                        i = map.indexOf(-1)
+//                    }
+//                    if (doesFit && i < originalMap.indexOf(window[0])) {
+//                        //println("Final i: $i")
+//                        for (k in 0 until window.size) {
+//                            map[originalMap.indexOf(window[0]) + k] = -1
+//                        }
+//                        for (k in 0 until window.size) {
+//                            map[k + i] = window[k]
+//                        }
+//                        //println(map)
+//                        break
+//                    }
+               }
+            }
+            result = originalMap.checkSum()
+        }
         return result
     }
 
@@ -68,6 +158,6 @@ fun main() {
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day09")
-    part1(input).println()
+    //part1(input).println()
     part2(input).println()
 }
